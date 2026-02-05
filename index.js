6391b3d3dee96789205c19eb2dcc2730e9ad89d4
muhammed ijas <ijaskarassery45@gmail.com>
2026-02-05 12:29:12 +0530

    update port

--- index.js
+++ index.js
@@ -433,7 +433,7 @@ app.put("/api/trades/:tradeId", async (req, res) => {
 // =========================
 // Server Start
 // =========================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(' Server running on port ${PORT}`);
 });

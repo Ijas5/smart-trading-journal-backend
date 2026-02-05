27a992289183febcad14a4394cca323f587e4155
muhammed ijas <ijaskarassery45@gmail.com>
2026-02-05 12:32:50 +0530

    railway 2

--- index.js
+++ index.js
@@ -435,5 +435,6 @@ app.put("/api/trades/:tradeId", async (req, res) => {
 // =========================
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(' Server running on port ${PORT}`);
+ console.log(`🚀 Server running on port ${PORT}`);
+
 });
